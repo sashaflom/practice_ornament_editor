3523f1b0daac27a7b8ae1765485a5b60e79c87f1
Oleksandra Flomboim <sflomboim@gmail.com>
2026-05-17 14:18:07 +0300

    add background for mainMenu.fxml

--- src/main/java/ornament_editor/Main.java
+++ src/main/java/ornament_editor/Main.java
@@ -1,4 +1,22 @@
 package ornament_editor;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ornament_editor/mainMenu.fxml"));
+        primaryStage.setTitle("Піксельна вишивка. Редактор орнаменту");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
